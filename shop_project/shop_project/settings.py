6c33f9c206dc332614204a8e544dd0d6879d7c8b
Сован В.Д. <slavasovan@gmail.com>
2025-08-13 14:43:43 +0300

    Switching to PostgreSQL and minor edits

--- shop_project/shop_project/settings.py
+++ shop_project/shop_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "main",
     "cart",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = "shop_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "carnivalrecords",
+        "USER": "carnivalrecords",
+        "PASSWORD": "carnivalrecords",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
