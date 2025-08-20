f551bda00c83d0a835922a1c9b8c84c303327884
Сован В.Д. <slavasovan@gmail.com>
2025-08-20 15:28:31 +0300

    Minor edits were added

--- shop_project/orders/models.py
+++ shop_project/orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from main.models import Product
 from users.models import User
 
@@ -16,6 +17,7 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
+    stripe_id = models.CharField(max_length=250, blank=True)
 
     class Meta:
         ordering = ["-created"]
@@ -26,6 +28,15 @@ class Order(models.Model):
 
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
+
+    def get_stripe_url(self):
+        if self.stripe_id:
+            return ""
+        if "__test__" in settings.STRIPE_SECRET_KEY:
+            path = "/test"
+        else:
+            path = "/"
+        return f"https://dashboard.stripe.com{path}/payments/{self.stripe_id}"
 
 
 class OrderItem(models.Model):
