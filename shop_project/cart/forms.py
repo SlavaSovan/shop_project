a4d443b6a5bb618fa1f213a9d9cdbed56e343b57
Сован В.Д. <slavasovan@gmail.com>
2025-08-20 17:19:00 +0300

    Made some changes and fixes

--- shop_project/cart/forms.py
+++ shop_project/cart/forms.py
@@ -5,8 +5,7 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 11)]
 
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
-        choices=PRODUCT_QUANTITY_CHOICES,
-        coerce=int,
+        choices=PRODUCT_QUANTITY_CHOICES, coerce=int, label="Количество:"
     )
     override = forms.BooleanField(
         required=False,
